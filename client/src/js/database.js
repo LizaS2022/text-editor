214fd927c1cd60bdd3cefe826ecb8540c93eda6c
Elizabeta Sirota <elizabetasirota@gmail.com>
2023-06-08 14:43:47 -0400

    polished code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-//indedb save information in the local machine
+
 export const putDb = async (content) => {
   
   const database = await openDB("jate", 1);
@@ -28,12 +27,8 @@ export const putDb = async (content) => {
   
   console.log(result);
 
-  // console.error('putDb not implemented');
-
 }
 
-// TODO: Add logic for a method that gets all the content from the database
-//gets information from the database. if the user still offline , the info continue to display
 export const getDb = async () => {
   const database = await openDB('jate', 1);
  
